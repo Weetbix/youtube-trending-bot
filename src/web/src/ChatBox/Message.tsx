a97b40de09a66f878fbd01b71113e8794209dbba
John Hannagan <weetbix_@hotmail.com>
2019-01-31 23:12:22 +0100

    Show response from API when typing

--- src/web/src/ChatBox/Message.tsx
+++ src/web/src/ChatBox/Message.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 
 import { componentFromStream } from 'recompose';
 import { from, ObservableInput } from 'rxjs';
-import { debounceTime, filter, map, pluck } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { debounceTime, filter, map, pluck, switchMap } from 'rxjs/operators';
 
 interface IProps {
     inputText: string;
 }
+
+const formatURL = (inputText: string) =>
+    `/generateMessage?replyTo=${encodeURI(inputText)}`;
 
 const Message = componentFromStream<IProps>(props$ => {
     const message$ = from(props$ as ObservableInput<IProps>).pipe(
         debounceTime(1000),
         pluck('inputText'),
         filter<string>(text => text.length > 0),
-        map(text => <span>{text}</span>),
+        map(formatURL),
+        switchMap(url =>
+            ajax(url).pipe(
+                pluck('response'),
+                pluck('message'),
+                map(text => <span>{text}</span>),
+            ),
+        ),
     );
 
     return message$;

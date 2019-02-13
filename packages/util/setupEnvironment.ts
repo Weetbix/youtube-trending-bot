1ee02d81e4860b17147a5acc79ccd7fd61112b5e
John Hannagan <weetbix_@hotmail.com>
2019-02-13 23:14:58 +0100

    Fixup util package

--- packages/util/setupEnvironment.ts
+++
@@ -1,9 +0,0 @@
-// Allow fetch in node
-import 'isomorphic-fetch';
-
-// Import environment variables from dotenv
-// We keep this in a separate file as there can be issues where the
-// environment is not initialized before a variable is used. See:
-// https://github.com/motdotla/dotenv/issues/133#issuecomment-255298822
-import { config } from 'dotenv';
-config();
